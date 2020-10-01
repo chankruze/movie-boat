4ffbe455bd39cf360e3d84b226d6edd3f0a7e569
chankruze <chankruze@gmail.com>
2020-10-01 23:20:55 +0530

    Adds Moive Querry
    Signed-off-by: chankruze <chankruze@gmail.com>

--- client/src/graphql/index.js
+++ client/src/graphql/index.js
@@ -25,6 +25,63 @@ const GQL_LATEST_MOVIES = gql`
   }
 `
 
+const GQL_MOVIE = gql`
+  query MovieQuerry($movie_id: Int!) {
+    movie(movie_id: $movie_id) {
+      id
+      url
+      imdb_code
+      title
+      title_english
+      title_long
+      slug
+      year
+      rating
+      runtime
+      genres
+      download_count
+      like_count
+      description_intro
+      description_full
+      yt_trailer_code
+      language
+      mpa_rating
+      background_image
+      background_image_original
+      small_cover_image
+      medium_cover_image
+      large_cover_image
+      medium_screenshot_image1
+      medium_screenshot_image2
+      medium_screenshot_image3
+      large_screenshot_image1
+      large_screenshot_image2
+      large_screenshot_image3
+      date_uploaded
+      date_uploaded_unix
+      torrents {
+        url
+        hash
+        quality
+        type
+        seeds
+        peers
+        size
+        size_bytes
+        date_uploaded
+        date_uploaded_unix
+      }
+      cast {
+        name
+        character_name
+        url_small_image
+        imdb_code
+      }
+    }
+  }
+`
+
 export default Object.freeze({
   GQL_LATEST_MOVIES,
+  GQL_MOVIE,
 })
