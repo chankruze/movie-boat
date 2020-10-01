81086e23a58b44df9cf50f3f2271420723d8d021
chankruze <chankruze@gmail.com>
2020-10-01 15:56:19 +0530

    Creates new Component PageNav
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ client/src/components/PageNav/PageBlock.js
@@ -0,0 +1,22 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Thu Oct 01 2020 12:45:22 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from 'react'
+import styles from './PageBlock.module.css'
+
+function PageBlock(props) {
+    const { pageNo, active, setPage } = props
+
+    return (
+        <div className={`${styles.pageBlock} ${active ? styles.active : ''}`}
+            onClick={() => setPage(pageNo)}>
+            {pageNo}
+        </div>
+    )
+}
+
+export default React.memo(PageBlock)
---
+++ client/src/components/PageNav/PageNav.js
@@ -0,0 +1,32 @@
+/*
+Author: chankruze (chankruze@geekofia.in)
+Created: Thu Oct 01 2020 11:56:32 GMT+0530 (India Standard Time)
+
+Copyright (c) Geekofia 2020 and beyond
+*/
+
+import React from 'react'
+import PageBlock from './PageBlock'
+import styles from './PageNav.module.css'
+
+function PageNav(props) {
+    let pages = []
+    const { totalPages, currentPage, setPage } = props
+
+    // console.log("Debug: ", totalPages, currentPage)
+
+    for (let index = 1; index <= totalPages; ++index) {
+        pages.push(
+            <PageBlock
+                key={index}
+                pageNo={index}
+                active={currentPage === index}
+                setPage={setPage}
+            />
+        )
+    }
+
+    return <div className={styles.pageNavWrapper}>{pages}</div>
+}
+
+export default React.memo(PageNav)
