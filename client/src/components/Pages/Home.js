a2239724c8d5e05554cf46582cd5086c5937c744
chankruze <chankruze@gmail.com>
2020-10-01 15:56:49 +0530

    Updates Home Page
    * Refactor movie list
    * Adds PageNav
    
    Signed-off-by: chankruze <chankruze@gmail.com>

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -6,43 +6,57 @@ Copyright (c) Geekofia 2020 and beyond
 */
 
 import React from 'react'
-import MovieCard from '../MovieCard/MovieCard'
+import { useQuery } from '@apollo/client'
+import querries from '../../graphql'
+
+// components
+import PageNav from '../PageNav/PageNav'
+import MovieGrid from '../MovieGrid/MovieGrid'
+
+// CSS
 import styles from './Home.module.css'
 
-const movieDetails = {
-    title: 'A Brilliant Young Mind',
-    year: '2014',
-    rating: 7.1,
-    genres: [
-        "Action",
-        "Comedy",
-        "Drama",
-        "Romance"
-    ],
-    coverImage: 'https://yts.mx/assets/images/movies/a_brilliant_young_mind_2014/medium-cover.jpg'
-}
-
 function Home() {
-    let movieArray = []
+    const [limit, setLimit] = React.useState(48)
+    const [page, setPage] = React.useState(1)
 
-    for (let index = 0; index < 23; index++) {
-        movieArray.push(movieDetails)
+    const { GQL_LATEST_MOVIES } = querries
+    const { loading, error, data } = useQuery(GQL_LATEST_MOVIES, { variables: { limit, page } })
+
+    if (loading) {
+        return (
+            <h4 className={styles.loader}>
+                Fetching Latest Movies...
+                <br/>
+                Page {page}
+            </h4>)
+    }
+
+    if (error) {
+        console.log(error)
+        return (
+            <h2 className={styles.loader}>
+                Error Fetching Latest Movies ;(
+            </h2>
+        )
+    }
+
+    if (data && data.movies) {
+        console.log(`movies updated at ${(new Date()).toLocaleTimeString().slice(0, 7)}`)
     }
 
     return (
         <div className={styles.homeWrapper}>
             {/* <h1>Latest</h1> */}
-            <div className={styles.movieList}>
-                {/* JS to generate list */}
-                {
-                    movieArray.map(movie => <MovieCard movieData={movie}/>)
-                }
-            </div>
-            <div className={styles.pageList}>
-                {/* JS to generate list */}
-            </div>
+            <MovieGrid movies={data.latestMovies.movies} />
+
+            {/* total_page_count = (movie_count / limit)
+                page_number = current_page */}
+            <PageNav totalPages={Math.floor(data.latestMovies.movie_count / data.latestMovies.limit)}
+                currentPage={data.latestMovies.page_number}
+                setPage={setPage} />
         </div>
     )
 }
 
-export default Home
+export default React.memo(Home)
